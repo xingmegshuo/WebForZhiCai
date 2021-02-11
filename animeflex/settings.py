3685e8944b7503a533939c49d95dcf66a1b018bd
xingmegshuo <xms.chnb@gmail.com>
2021-02-11 08:57:00 +0800

    first

--- animeflex/settings.py
+++ animeflex/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'animeflex.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.sqlite',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
