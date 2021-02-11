f45f44b4ce325d9e5bde94c62456bc7eefc02bea
xingmegshuo <xms.chnb@gmail.com>
2021-02-11 10:37:16 +0800

    db

--- animeflex/settings.py
+++ animeflex/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'modeltranslation',
+    # 'modeltranslation',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
