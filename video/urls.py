7b1bdc50b5edb6619539ebd95daaf6b7431dbd17
xingmegshuo <xms.chnb@gmail.com>
2021-02-26 15:28:06 +0800

    update

--- video/urls.py
+++ video/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, re_path
-from .views import home_view, login, video
+from .views import home_view, login, video, mobile_home,mobile_video
 
 urlpatterns = [
     path('', home_view),
+    path('index.m',mobile_home),
     path('login', login),
     re_path(r'^(\d+)/$', video),
+    re_path(r'^m(\d+)/$',mobile_video),
 ]
--- video/views.py
+++ video/views.py
@@ -17,6 +17,16 @@ def home_view(request):
         return render(request, 'video/home.html', {"desc": desc, "video": video})
 
 
+def mobile_home(request):
+    desc = Desc.objects.all()
+    video = Video.objects.all()
+    if 'name' in request.COOKIES:
+        name = request.COOKIES['name']
+        return render(request, 'mobile/home.html', {"name": name, "desc": desc, "video": video})
+    else:
+        return render(request, 'mobile/home.html', {"desc": desc, "video": video})
+
+
 # 登录
 def login(request):
     # md5 = hashlib.md5()
@@ -39,3 +49,12 @@ def video(request, id):
         return render(request, 'video/palyVido.html', {'video': v,'name':name})
     else:
         return render(request, 'video/palyVido.html', {'video': v})
+
+
+def mobile_video(request,id):
+    v = Video.objects.get(id=id)
+    if 'name' in request.COOKIES:
+        name = request.COOKIES['name']
+        return render(request, 'mobile/palyVido.html', {'video': v,'name':name})
+    else:
+        return render(request, 'mobile/palyVido.html', {'video': v})
